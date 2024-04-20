4277f5fb7e0e317277700c0d1385aa47f6a03208
msuad <1445617868@qq.com>
2024-04-20 20:44:45 +0800

    update

---
+++ 1.py
@@ -0,0 +1,13 @@
+import socket
+def getipv6():
+    host_ipv6=[]
+    ips=socket.getaddrinfo(socket.gethostname(),80)
+    for ip in ips:
+        if ip[4][0].startswith('24'):
+    #2408 中国联通
+    #2409 中国移动
+    #240e 中国电信
+    #        print(ip[4][0])
+            host_ipv6.append(ip[4][0])
+    return host_ipv6
+getipv6()
---
+++ autorun.py
@@ -0,0 +1,7 @@
+from time import *
+import os
+
+
+while True:
+    a=os.system("f:/Tencentcloud_IPv6_DDNS/venv/Scripts/python.exe f:/Tencentcloud_IPv6_DDNS/DDNS_v3.py")  #使用a接收返回值
+    sleep(60)
